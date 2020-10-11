ed78fc3fa25d660d8f0c7146bd7b43c6ab1ff68f
Anton Zhygariev <anton.zhyharyev@gmail.com>
2020-10-11 19:04:12 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -2,22 +2,10 @@ const ingredients = ["Картошка", "Грибы", "Чеснок", "Поми
 
 const ingredientsEl = document.querySelector("#ingredients");
 
-const potato = document.createElement("li");
-potato.textContent = ingredients[0];
+const ingredient = ingredients.map((el, i) => {
+	el = document.createElement("li");
+	el.textContent = ingredients[i];
+	return el;
+})
 
-const mushrooms = document.createElement("li");
-mushrooms.textContent = ingredients[1];
-
-const garlic = document.createElement("li");
-garlic.textContent = ingredients[2];
-
-const tomato = document.createElement("li");
-tomato.textContent = ingredients[3];
-
-const greens = document.createElement("li");
-greens.textContent = ingredients[4];
-
-const condiments = document.createElement("li");
-condiments.textContent = ingredients[5];
-
-ingredientsEl.append(potato, mushrooms, garlic, tomato, greens, condiments);
+ingredientsEl.append(...ingredient);
