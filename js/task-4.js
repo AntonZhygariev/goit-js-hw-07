9e86edcacb395d202b44b17cd058b6eb8743e50b
Anton Zhygariev <anton.zhyharyev@gmail.com>
2020-10-11 19:04:16 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -2,8 +2,9 @@ const buttonDecEl = document.querySelector('button[data-action="decrement"]');
 const buttonIncEl = document.querySelector('button[data-action="increment"]');
 let valEl = document.querySelector("#value");
 
-const onDecBtnClick = () => (valEl.textContent = Number(valEl.textContent) - 1);
-const onIncBtnClick = () => (valEl.textContent = Number(valEl.textContent) + 1);
+// const onDecBtnClick = () => (valEl.textContent = Number(valEl.textContent) - 1);
+const onDecBtnClick = () => --valEl.textContent;
+const onIncBtnClick = () => ++valEl.textContent;
 
 buttonDecEl.addEventListener("click", onDecBtnClick);
 buttonIncEl.addEventListener("click", onIncBtnClick);
